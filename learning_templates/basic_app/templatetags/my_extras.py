2b2315f14a7c8dcf3c736274e497b853883db668
smart0586 <smart0586@gmail.com>
2021-11-19 13:26:56 -0800

    fixed import issue

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -1,12 +1,6 @@
-from django import relative_url_templates
+from django import template
 register = template.Library()
 
-@register.filter(name='cut')
-def cut(value,arg):
-    """
-    This cuts out all values of arg from the string
-    """
-
-    return value.replace(arg,'')
-
-# register.filter('cut',cut)
+@register.filter
+def cutout(value, arg):
+    return value.replace(arg, '')
